b46b1e8e1d180478c2e08478c8f534374bcd87c8
arun436 <arunkumarchintha136@gmail.com>
2020-12-16 13:53:03 +0530

    inserting first java file Arun.java

--- Arun.java
+++ Arun.java
@@ -1,1 +1,31 @@
 
+import java.util.Scanner;
+public class Arun 
+{ 
+    public static void rightTriangle(int n) 
+    { 
+        int i;  
+        for(i=1; i<=n; i++)
+        {
+        	for(int k=n-1;k>=i;k--)
+        	{
+        		System.out.print(" ");
+        	}
+        	for(int j=1;j<=i;j++)
+        	{
+        		System.out.print("* ");
+        	}
+        	System.out.println();
+        } 
+    }
+     
+    public static void main(String args[]) 
+    { 
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n value");
+        int n = sc.nextInt();
+        sc.close();
+        Arun a = new Arun();
+        a.rightTriangle(n);
+    } 
+}
